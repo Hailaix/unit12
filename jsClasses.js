03a1cdcb659e85f5dac26bb6920dcc6b3517d7a8
NHower <howernickoli@gmail.com>
2022-07-11 21:28:06 -0400

    vehicle class part 1

---
+++ jsClasses.js
@@ -0,0 +1,13 @@
+class Vehicle {
+    constructor(make,model,year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    honk(){
+        return "Beep";
+    }
+    toString(){
+        return `The vehicle is a ${make} ${model} from ${year}`;
+    }
+}
